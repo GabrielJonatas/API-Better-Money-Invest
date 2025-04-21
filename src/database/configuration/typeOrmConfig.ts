d90adae4ad698af1e11385b2588ae43d861695cd
GabrielJonatas <gabrieljonatas2000@hotmail.com>
2025-04-21 15:09:20 -0300

    feat: update database configuration for development and production environments

--- src/database/configuration/typeOrmConfig.ts
+++ src/database/configuration/typeOrmConfig.ts
@@ -1,8 +1,20 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const configuration = {
+export const dev_configuration = {
   type: 'sqlite',
   database: './src/database/db/sqlite.db',
+  autoLoadEntities: true,
+  synchronize: true,
+} as TypeOrmModuleOptions;
+
+export const prod_configuration = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [],
   autoLoadEntities: true,
   synchronize: true,
 } as TypeOrmModuleOptions;
